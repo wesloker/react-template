077a51bb367fb2a4fa63aa7c4267b9f9a298a83c
wesloker <wesloker.aqs@gmail.com>
2019-05-30 15:03:30 -0500

    Initial commit from SPD App

---
+++ src/components/App/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Page from './page';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <Page />
+    );
+  }
+}
+
+export default App;
---
+++ src/components/App/page.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './index.css';
+
+function Page(props) {
+  return (
+    <span>Content page here</span>
+  );
+}
+
+export default Page;
